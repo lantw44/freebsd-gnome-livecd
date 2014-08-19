27ff34788c8d11fb4b88fc042c6c7392d95c3ce0
Ting-Wei Lan <lantw@src.gnome.org>
2014-08-20 01:52:43 +0800

    Remove the micro version number in the name of output files
    Not all GNOME modules release at the same time, so it is not easy to make
    a suitable micro version number to represent the entire GNOME desktop.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 : ${src:="/home/lantw44/livecd/src"}
 : ${root:="/home/lantw44/livecd/root"}
 : ${cdroot:="/home/lantw44/livecd/cdroot"}
-: ${image:="/home/lantw44/livecd/out/FreeBSD-10.0-GNOME-3.12.2-`date '+%Y%m%d'`.iso"}
+: ${image:="/home/lantw44/livecd/out/FreeBSD-10.0-GNOME-3.12-`date '+%Y%m%d'`.iso"}
 
 : ${repo="/usr/local/poudriere/data/packages/freebsd10-marcuscom-gnome3"}
 : ${pkgs="`cat /home/lantw44/livecd/gnome3-pkgs`"}
